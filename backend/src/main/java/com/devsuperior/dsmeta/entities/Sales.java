42ef0515c8cab8c84da485c67fe7587233580c35
Rodrigo Milanez <rodrigo.ohland@hotmail.com>
2022-07-13 20:04:47 -0300

    API Rest

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sales.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tb_sales")
+public class Sales {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String seller_name;
+	private Integer visited;
+	private Integer deals;
+	private Double amount; 
+	private LocalDate date;
+	
+	public Sales() {
+		
+	}
+	
+	public Sales(Long id, String seller_name, Integer visited, Integer deals, Double amount, LocalDate date) {
+		super();
+		this.id = id;
+		this.seller_name = seller_name;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+	
+	public String getSeller_name() {
+		return seller_name;
+	}
+	public void setSeller_name(String seller_name) {
+		this.seller_name = seller_name;
+	}
+	public Integer getVisited() {
+		return visited;
+	}
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+	public Integer getDeals() {
+		return deals;
+	}
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+}
